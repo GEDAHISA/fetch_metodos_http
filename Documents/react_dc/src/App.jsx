f7e8b707dd18812c807d007b133cf09afeaee113
GEDAHISA <106118710+GEDAHISA@users.noreply.github.com>
2023-04-17 21:50:11 -0300

    criando navbar

--- Documents/react_dc/src/App.jsx
+++ Documents/react_dc/src/App.jsx
@@ -1,6 +1,7 @@
 
-import './App.css'
+
 import { InputText } from './components/InputText'
+import { MYNavbar } from './components/MYNavbar'
 import { H1Title } from './components/h1Title'
 
 function App() {
@@ -8,26 +9,7 @@ function App() {
 
   return (
     <>
-       <H1Title titulo='abacaxi torrado' />
-      {/* <h1>Olá Mundo</h1>
-      <h1>Abacaxi Torrado</h1> */}
-
-      <InputText
-        id='nome'
-        name='nome'
-        placeholder='nome'
-      />
-      <br />
-      <InputText
-        id='sobrenome'
-        name='sobrenome'
-        placeholder='sobrenome'
-      />
-      {/*       
-      <input type="text" name="nome" id="nome" placeholder='nome'></input>
-      <br />
-      <input type="text" name="sobrenome" id="sobrenome" placeholder='nome'></input>
- */}
+    <MYNavbar/>
 
     </>
   )
